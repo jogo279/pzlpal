4c85fc3b524bf3508d2a12fec96983e4563ce284
Jon <jogo279@gmail.com>
2015-07-20 12:46:18 -0400

    cron cleanup scripts

--- server/digitize.js
+++ server/digitize.js
@@ -406,39 +406,9 @@ function similar_to(num, str) {
   return true;
 }
 
-/* http://stackoverflow.com/questions/18052762/remove-directory-which-is-not-empty */
-function remove_folder(location, next) {
-    fs.readdir(location, function (err, files) {
-        async.each(files, function (file, cb) {
-            file = location + '/' + file
-            fs.stat(file, function (err, stat) {
-                if (err) {
-                    return cb(err);
-                }
-                if (stat.isDirectory()) {
-                    removeFolder(file, cb);
-                } else {
-                    fs.unlink(file, function (err) {
-                        if (err) {
-                            return cb(err);
-                        }
-                        return cb();
-                    })
-                }
-            })
-        }, function (err) {
-            if (err) return next(err)
-            fs.rmdir(location, function (err) {
-                return next(err)
-            })
-        })
-    })
-}
-
 function clean_up(puzzle, err) {
   console.log(err);
   puzzle.digitizing_status = "failure";
   puzzle.save();
-  remove_folder(require('./config').app_dir + '/public/images/' + puzzle._id + '/', function(err) { console.log(err)});
   return;
 }
